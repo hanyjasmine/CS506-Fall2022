d2bcad7f5c0fb3817745479823d7e19e929a86dc
hanyjasmine <hjasmine@bu.edu>
2022-09-15 18:05:19 -0400

    updated infrastructure

--- labs/01-lab/infrastructure/power.py
+++ labs/01-lab/infrastructure/power.py
@@ -1,3 +1,11 @@
 def draw_power_plant():
-    print("power plant not found")
+    print("     __                           __     ")
+    print("    |  |        __________       |  |     ")
+    print("    |  |       |          |      |  |     ")
+    print("    |  |      |____________|     |  |     ")
+    print("    |  |      |            |     |  |     ")
+    print("    |  |      |____________|     |  |     ")
+    print("    |  |      |     __     |     |  |     ")
+    print("    |  |      |    |  |    |     |  |     ")
+    print("    |__|      |____|__|____|     |__|     ")
     return
