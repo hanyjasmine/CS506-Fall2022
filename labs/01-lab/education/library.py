921425b84040da7928c3e099b7d34dc50000ec2f
hanyjasmine <hjasmine@bu.edu>
2022-09-15 16:04:59 -0400

    updated library

--- labs/01-lab/education/library.py
+++ labs/01-lab/education/library.py
@@ -1,3 +1,10 @@
 def draw_library():
-    print("library not found")
+    print("       _______        ")
+    print("      /       \       ")
+    print("     /_________\      ")
+    print("    |           |     ")
+    print("    |           |     ")
+    print("    |     _     |     ")
+    print("    |____| |____|     ")
+    print("    ")
     return
