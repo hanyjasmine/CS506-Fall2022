861bebf2cf51e0dadbb03c87b0d3c8b611b99604
hanyjasmine <hjasmine@bu.edu>
2022-09-22 15:55:45 -0400

    added lab2 functions

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,20 @@
+import csv 
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    
+    with open(csv_file_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row[0] in MONTHS:
+                sub = [int(row[i]) for i in range(1, len(row))]
+                row = [row[0]] + sub
+            else:
+                row = [int(row[i]) for i in range(len(row))]
+            res.append(row)
+    return res
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,5 @@
+import math 
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +7,30 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 or len(y) == 0:
+        raise ValueError("lengths must not be zero")
+    s1 = set(x)
+    s2 = set(y)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) == 0 or len(y) == 0:
+        raise ValueError("lengths must not be zero")
+    elif len(x) != len(y) == 0:
+        raise ValueError("lengths must be equal")
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(x)):
+        val1 = x[i]
+        val2 = y[i]
+        sumxx += val1 * val1
+        sumyy += val2 * val2
+        sumxy += val1 * val2
+    return sumxy/math.sqrt(sumxx*sumyy)
 
 # Feel free to add more
